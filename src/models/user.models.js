a45ba449398659b2c7ef9447d6ba2db956172faf
Avanish-Kumar123 <avanis4309@gmail.com>
2025-06-23 16:19:00 +0530

    add user and video model

---
+++ src/models/user.models.js
@@ -0,0 +1,19 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true // purpose searching wagara
+        }
+    }
+)
+
+
+
+
+export const User = mongoose.model("User", userSchema)
